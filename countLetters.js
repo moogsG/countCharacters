fd61b0767e741a40bff23f950a8160be8e7aa586
Morgan Greff <morgan@greffs.com>
2017-09-06 16:24:27 +0000

    Revert back to first commit

--- countLetters.js
+++ countLetters.js
@@ -1,31 +1,26 @@
-
 //Decaring vars for global use
 
 var letterCount;
 var letter;
-var letterPosition = [];
 var output = { //decares my object
       letter: letter,
-      count: letterCount,
-      position: letterPosition
+      count: letterCount
     };
 
 
 function countLetters(str) {
 
-  str = str.toLowerCase().split(" ").join(" "); //makes it a single array
+  str = str.toLowerCase().split(" ").join(""); //makes it a single array
 
   for (var i = 0; i < str.length; i++) { //seperates letters and sends them to comapreLetters
 
     letterCount = 0; //resets lettercount
     letter = str[i];
-    letterPosition = [];
 
-    compareLetters(letter, str); //sends to compare letter function.  Letter looks like it is miss spelt at this point.
+    compareLetters(letter, str);
 
     output['letter'] = letter;// adds values to object
     output['count'] = letterCount;
-    output['position'] = letterPosition;
 
     console.log(output);
 
@@ -40,7 +35,7 @@ function compareLetters(letter, str) { //compares the letters
     var letterTwo = str[x];
 
     if (letterTwo == letter) {
-      letterPosition.push(x);
+
       letterCount += 1; //adds to the count for every duplicate.
 
     }
